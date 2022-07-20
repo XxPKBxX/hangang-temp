2ae690645bf744e1a336971948b8265cbf079110
pkb <pkb@kakao.com>
2022-07-20 18:44:35 +0900

    State fixes

--- pages/index.tsx
+++ pages/index.tsx
@@ -30,23 +30,22 @@ export const getServerSideProps: GetServerSideProps = async () => {
 }
 
 const Home: NextPage<{ data: HangangAPIResponse }> = ({ data }) => {
-  if (data !== null && data.WPOSInformationTime.RESULT.CODE === 'INFO-000') {
-    const [current, setCurrent] = useState<number>(0)
+  const [current, setCurrent] = useState<number>(0)
+  
+  if (data === null || data.WPOSInformationTime.RESULT.CODE !== 'INFO-000') return <ErrorWindow message={'정보를 불러오는 데 실패하였습니다.\n잠시 후에 다시 시도하여 주세요.'} />
 
-    const value: DefaultValueData = {
-      data: data.WPOSInformationTime.row,
-      settings: {
-        current: [current, setCurrent]
-      }
+  const value: DefaultValueData = {
+    data: data.WPOSInformationTime.row,
+    settings: {
+      current: [current, setCurrent]
     }
-
-    return (
-      <TempContext.Provider value={value}>
-        <HangangData />
-      </TempContext.Provider>
-    )
   }
-  else return <ErrorWindow message={'정보를 불러오는 데 실패하였습니다.\n잠시 후에 다시 시도하여 주세요.'} />
+
+  return (
+    <TempContext.Provider value={value}>
+      <HangangData />
+    </TempContext.Provider>
+  )
 }
 
 export default Home
